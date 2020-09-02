45428a1ed7199ee90186654432948fcd66e7bf90
此昵称不存在 <878740953@qq.com>
2020-09-02 15:57:10 +0800

    git测试01

---
+++ maven_service/src/main/java/com/ssm/service/impl/AccountServiceImpl.java
@@ -0,0 +1,19 @@
+package com.ssm.service.impl;
+
+import com.ssm.dao.AccountDao;
+import com.ssm.domain.Account;
+import com.ssm.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+public class AccountServiceImpl implements AccountService {
+    @Autowired
+    private AccountDao accountDao;
+    public List<Account> findAllAccount() {
+        return accountDao.findAllAccount();
+    }
+
+    public void saveAccount(Account account) {
+        accountDao.saveAccount(account);
+    }
+}
